fc35f9bbd77802af4bf464c2894d07ad10fe155c
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2023-01-21 17:44:35 -0600

    rate of change

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,12 +8,23 @@ count = 0
 accum = 0
 with open('Resources/budget_data.csv') as file:
     lines = file.readlines()
+    
+    lst =[]
 
     for line in lines[1:]:
         accum += int(line.strip().split(',')[1])
         count += 1
+        lst.append(int(line.strip().split(',')[1]))
         print(line)
+
+    max_check = list(lst)[0]
+
+    for elem in lst:
+        if elem > max_check:
+            max_check = elem
+
 print(accum)
 print(count)    
+print(count)
+print(max_check)
 
-print(accum/count)
